84a7be8211a93ecc63d28a99275d79fbd202e409
Yahya9696 <yahieadawa@gmail.com>
2025-12-09 21:36:49 +0300

    Update config_sample.py
    Signed-off-by: Yahya9696 <yahieadawa@gmail.com>

--- config_sample.py
+++ config_sample.py
@@ -43,8 +43,8 @@ DISABLE_SEED = False
 DISABLE_FF_MODE = False
 
 # Telegraph
-AUTHOR_NAME = "WZML-X"
-AUTHOR_URL = "https://t.me/WZML_X"
+AUTHOR_NAME = "Yahya"
+AUTHOR_URL = "https://t.me/i1_6_9"
 
 # Task Limits
 DIRECT_LIMIT = 0
@@ -109,7 +109,7 @@ STOP_DUPLICATE = False
 INDEX_URL = ""
 
 # YT Tools
-YT_DESP = "Uploaded to YouTube by WZML-X bot"
+YT_DESP = "Uploaded to YouTube by LeexhYL bot"
 YT_TAGS = ["telegram", "bot", "youtube"]  # or as a comma-separated string
 YT_CATEGORY_ID = 22
 YT_PRIVACY_STATUS = "unlisted"
